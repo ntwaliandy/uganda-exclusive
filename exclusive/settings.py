26ec6b693ff1ba41e188bb4e87803c457879f194
andy250 <ntwaliandy90@gmail.com>
2021-06-06 14:23:17 +0300

    latest upload

--- exclusive/settings.py
+++ exclusive/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6i1t87wn)9#30p0^&cli@agov#w=-uc6lz1f4nnt$hh%o^6xba'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['uga-exclusive.herokuapp.com', '127.0.0.1']
 
